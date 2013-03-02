7ded0e90f6366467dac0a4433fa168c22e03da53
Rafal Zajac <rzajac@gmail.com>
2013-03-02 13:33:45 +0100

    GDG Krakow AppEngine

--- slide_config.js
+++ slide_config.js
@@ -1,14 +1,14 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Google App Engine',
+    subtitle: 'Python',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    // favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -18,12 +18,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Rafal Zajac',
+    company: 'CTO, Co-founder of <a href="http://radioflag.com">RadioFlag.com</a>',
+    gplus: 'https://plus.google.com/117585488707006432442',
+    twitter: '@zajacr',
+    // www: 'http://www.radioflag.com',
+    // github: 'http://github.com/rzajac'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
